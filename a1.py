d5570c4db9453a4188920551155c13a4c7383b96
prasanthyalla <57980279+prasanthyalla@users.noreply.github.com>
2019-11-20 15:57:17 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,8 @@
+#This program adds two numbers provided by the user.
+# Store input numbers.
+num1 = 41
+num2 = 30
+# Add two numbers.
+sum = float(num1) + float(num2)
+# Display the sum.
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
